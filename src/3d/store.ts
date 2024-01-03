7460e0f7193b11bb94da7377365032d0996b9a52
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-01-03 10:48:45 -0500

    Create store.ts

---
+++ src/3d/store.ts
@@ -0,0 +1,20 @@
+import create from 'zustand';
+import * as Ammo from 'ammo.js';
+
+type State = {
+  physicsWorld: Ammo.btDiscreteDynamicsWorld | null;
+  addRigidBody: (body: Ammo.btRigidBody) => void;
+  // ... other state and actions
+};
+
+export const useStore = create<State>((set) => ({
+  physicsWorld: null,
+  // ... other state initializations
+  addRigidBody: (body) => set((state) => {
+    if (state.physicsWorld) {
+      state.physicsWorld.addRigidBody(body);
+      // You could also keep track of these bodies if needed
+    }
+  }),
+  // ... other actions
+}));
