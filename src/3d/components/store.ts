24e44e7a9a390fd31ee4b5c4396ac72706560333
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-01-05 12:11:39 -0500

    config issues needed to fix

--- src/3d/components/store.ts
+++ src/3d/components/store.ts
@@ -1,3 +1,19 @@
+// import { create } from 'zustand';
+// import * as THREE from 'three';
+
+// type State = {
+//   testMesh: InstanceType<typeof THREE.Mesh>;
+// };
+
+// export const useStore = create<State>(() => ({
+//   testMesh: new THREE.Mesh(),
+// }));
+
+
+
+
+
+
 import { create } from 'zustand';
 import * as THREE from "three";
 
