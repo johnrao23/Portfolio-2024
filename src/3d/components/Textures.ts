1deff7ada8e95812aee15a20997e32167d2ec851
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-01-08 12:45:39 -0500

    Update Textures.ts
    added types

--- src/3d/components/Textures.ts
+++ src/3d/components/Textures.ts
@@ -1,56 +1,112 @@
-//billboardTextures
-let billboardTextures = {};
-billboardTextures.terpSolutionsTexture = "../src/jsm/terpSolutions.png";
-billboardTextures.bullVsBearTexture = "../src/jsm/bvbbillboard.png";
-billboardTextures.fairbnbTexture = "../src/jsm/fairbnb.png";
-billboardTextures.scanAndGoTexture = "../src/jsm/SaG-billboard.png";
+// Define the structure for billboard textures
+interface BillboardTextures {
+  terpSolutionsTexture: string;
+  bullVsBearTexture: string;
+  fairbnbTexture: string;
+  scanAndGoTexture: string;
+}
 
-//box textures
-let boxTexture = {};
-boxTexture.Github = "../src/jsm/githubLogo.png";
-boxTexture.twitter = "../src/jsm/twitterLogo.png";
-boxTexture.LinkedIn = "../src/jsm/linkedInLogo.png";
-boxTexture.mail = "../src/jsm/envelope.png";
-boxTexture.globe = "../src/jsm/thunder.png";
-boxTexture.reactIcon = "../src/jsm/react.png";
-boxTexture.allSkills = "../src/jsm/allSkills.png";
-boxTexture.lensFlareMain = "../src/jsm/lensflare0.png";
-boxTexture.lucasNoah = "../src/jsm/LucasNoahPic.jpeg";
-boxTexture.familyText = "../src/jsm/Family.png";
-boxTexture.writing = "../src/jsm/writing.png";
+// Define billboard textures with updated paths
+let billboardTextures: BillboardTextures = {
+  terpSolutionsTexture: "../assets/png/terpSolutions.png",
+  bullVsBearTexture: "../assets/png/bvbbillboard.png",
+  fairbnbTexture: "../assets/png/fairbnb.png",
+  scanAndGoTexture: "../assets/png/SaG-billboard.png",
+};
 
-//material textures
-let stoneTexture = "../src/jsm/stone.png";
-let woodTexture = "../src/jsm/woodTexture.jpg";
+// Define the structure for box textures
+interface BoxTexture {
+  Github: string;
+  twitter: string;
+  LinkedIn: string;
+  mail: string;
+  globe: string;
+  reactIcon: string;
+  allSkills: string;
+  lensFlareMain: string;
+  lucasNoah: string;
+  familyText: string;
+  writing: string;
+}
 
-//text
-let inputText = {};
-inputText.terpSolutionsText = "../src/jsm/terp-solutions-text.png";
-inputText.activities = "../src/jsm/activities_text.png";
-inputText.bullVsBearText = "../src/jsm/bvb-text.png";
-inputText.fairbnbText = "../src/jsm/fairbnb-text.png";
-inputText.scanAndGoText = "../src/jsm/SaG-text.png";
-inputText.staticPortfolio = "../src/jsm/static-portfolio.png";
+// Define box textures with updated paths
+let boxTexture: BoxTexture = {
+  Github: "../assets/png/githubLogo.png",
+  twitter: "../assets/png/twitterLogo.png",
+  LinkedIn: "../assets/png/linkedInLogo.png",
+  mail: "../assets/png/envelope.png",
+  globe: "../assets/png/thunder.png",
+  reactIcon: "../assets/png/react.png",
+  allSkills: "../assets/png/allSkills.png",
+  lensFlareMain: "../assets/png/lensflare0.png",
+  lucasNoah: "../assets/jpg/LucasNoahPic.jpeg",
+  familyText: "../assets/png/Family.png",
+  writing: "../assets/png/writing.png",
+};
 
-//SVG
-let SVG = {};
-SVG.reactLogo = "../src/jsm/react-svg.svg";
+// Define material textures with updated paths
+let stoneTexture: string = "../assets/png/stone.png";
+let woodTexture: string = "../assets/jpg/woodTexture.jpg";
 
-//URLs
-let URL = {};
-URL.terpsolutions =
-  "https://web.archive.org/web/20200302001846/https://terpsolutions.com/";
-URL.johnrao = "https://johnrao.com";
-URL.bullVsBearTrading = "https://www.bullvsbeartrading.com";
-URL.getFairbnb = "https://www.getfairbnb.com";
-URL.samsClubURL = "https://www.samsclub.com/";
-URL.gitHub = "https://github.com/johnrao23/Portfolio-2022";
-URL.twitter = "https://twitter.com/0xRao";
-URL.LinkedIn = "https://www.linkedin.com/in/john-rao";
-URL.email = "https://mailto:johnrao23@gmail.com";
-URL.githubBullVsBear = "https://github.com/johnrao23/bull-vs-bear-trading";
-URL.samsClub = "https://www.samsclub.com/";
+// Define the structure and values for input text
+interface InputText {
+  terpSolutionsText: string;
+  activities: string;
+  bullVsBearText: string;
+  fairbnbText: string;
+  scanAndGoText: string;
+  staticPortfolio: string;
+}
 
+// Define input text with updated paths
+let inputText: InputText = {
+  terpSolutionsText: "../assets/png/terp-solutions-text.png",
+  activities: "../assets/png/activities_text.png",
+  bullVsBearText: "../assets/png/bvb-text.png",
+  fairbnbText: "../assets/png/fairbnb-text.png",
+  scanAndGoText: "../assets/png/SaG-text.png",
+  staticPortfolio: "../assets/png/static-portfolio.png",
+};
+
+// Define SVG with updated paths
+interface SVG {
+  reactLogo: string;
+}
+
+let SVG: SVG = {
+  reactLogo: "../assets/svg/react-svg.svg",
+};
+
+// Define URLs
+interface URL {
+  terpsolutions: string;
+  johnrao: string;
+  bullVsBearTrading: string;
+  getFairbnb: string;
+  samsClubURL: string;
+  gitHub: string;
+  twitter: string;
+  LinkedIn: string;
+  email: string;
+  githubBullVsBear: string;
+  samsClub: string;
+}
+
+let URL: URL = {
+  terpsolutions: "https://web.archive.org/web/20200302001846/https://terpsolutions.com/",
+  johnrao: "https://johnrao.com",
+  bullVsBearTrading: "https://www.bullvsbeartrading.com",
+  getFairbnb: "https://www.getfairbnb.com",
+  samsClubURL: "https://www.samsclub.com/",
+  gitHub: "https://github.com/johnrao23/Portfolio-2022",
+  twitter: "https://twitter.com/0xRao",
+  LinkedIn: "https://www.linkedin.com/in/john-rao",
+  email: "mailto:johnrao23@gmail.com",
+  githubBullVsBear: "https://github.com/johnrao23/bull-vs-bear-trading",
+  samsClub: "https://www.samsclub.com/",
+};
+
+// Export all defined structures
 export {
   billboardTextures,
   boxTexture,
@@ -58,4 +114,5 @@ export {
   URL,
   stoneTexture,
   woodTexture,
+  SVG
 };
