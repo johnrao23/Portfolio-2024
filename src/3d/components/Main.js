524d8f0b8b1f86ac78e4fe051a6e4ae8e9d6dd96
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-01-09 11:11:46 -0500

    Update Main.js

--- src/3d/components/Main.js
+++ src/3d/components/Main.js
@@ -168,191 +168,191 @@ const Main = () => {
     }
 
     // create ball
-    function createBall() {
-      let pos = { x: 8.75, y: 0, z: 0 };
-      let radius = 2;
-      let quat = { x: 0, y: 0, z: 0, w: 1 };
-      let mass = 3;
+    // function createBall() {
+    //   let pos = { x: 8.75, y: 0, z: 0 };
+    //   let radius = 2;
+    //   let quat = { x: 0, y: 0, z: 0, w: 1 };
+    //   let mass = 3;
 
-      var marble_loader = new THREE.TextureLoader(manager);
-      var marbleTexture = marble_loader.load("./src/jsm/earth.jpg");
-      marbleTexture.wrapS = marbleTexture.wrapT = THREE.RepeatWrapping;
-      marbleTexture.repeat.set(1, 1);
-      marbleTexture.anisotropy = 1;
-      marbleTexture.encoding = THREE.sRGBEncoding;
+    //   var marble_loader = new THREE.TextureLoader(manager);
+    //   var marbleTexture = marble_loader.load("./src/jsm/earth.jpg");
+    //   marbleTexture.wrapS = marbleTexture.wrapT = THREE.RepeatWrapping;
+    //   marbleTexture.repeat.set(1, 1);
+    //   marbleTexture.anisotropy = 1;
+    //   marbleTexture.encoding = THREE.sRGBEncoding;
 
-      //threeJS Section
-      let ball = (ballObject = new THREE.Mesh(
-        new THREE.SphereGeometry(radius, 32, 32),
-        new THREE.MeshLambertMaterial({ map: marbleTexture }),
-      ));
+    //   //threeJS Section
+    //   let ball = (ballObject = new THREE.Mesh(
+    //     new THREE.SphereGeometry(radius, 32, 32),
+    //     new THREE.MeshLambertMaterial({ map: marbleTexture }),
+    //   ));
 
-      ball.geometry.computeBoundingSphere();
-      ball.geometry.computeBoundingBox();
+    //   ball.geometry.computeBoundingSphere();
+    //   ball.geometry.computeBoundingBox();
 
-      ball.position.set(pos.x, pos.y, pos.z);
+    //   ball.position.set(pos.x, pos.y, pos.z);
 
-      ball.castShadow = true;
-      ball.receiveShadow = true;
+    //   ball.castShadow = true;
+    //   ball.receiveShadow = true;
 
-      scene.add(ball);
+    //   scene.add(ball);
 
-      //Ammojs Section
-      let transform = new Ammo.btTransform();
-      transform.setIdentity();
-      transform.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
-      transform.setRotation(
-        new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w),
-      );
-      let motionState = new Ammo.btDefaultMotionState(transform);
+    //   //Ammojs Section
+    //   let transform = new Ammo.btTransform();
+    //   transform.setIdentity();
+    //   transform.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
+    //   transform.setRotation(
+    //     new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w),
+    //   );
+    //   let motionState = new Ammo.btDefaultMotionState(transform);
 
-      let colShape = new Ammo.btSphereShape(radius);
-      colShape.setMargin(0.05);
+    //   let colShape = new Ammo.btSphereShape(radius);
+    //   colShape.setMargin(0.05);
 
-      let localInertia = new Ammo.btVector3(0, 0, 0);
-      colShape.calculateLocalInertia(mass, localInertia);
+    //   let localInertia = new Ammo.btVector3(0, 0, 0);
+    //   colShape.calculateLocalInertia(mass, localInertia);
 
-      let rbInfo = new Ammo.btRigidBodyConstructionInfo(
-        mass,
-        motionState,
-        colShape,
-        localInertia,
-      );
-      let body = new Ammo.btRigidBody(rbInfo);
-      //body.setFriction(4);
-      body.setRollingFriction(10);
+    //   let rbInfo = new Ammo.btRigidBodyConstructionInfo(
+    //     mass,
+    //     motionState,
+    //     colShape,
+    //     localInertia,
+    //   );
+    //   let body = new Ammo.btRigidBody(rbInfo);
+    //   //body.setFriction(4);
+    //   body.setRollingFriction(10);
 
-      //set ball friction
+    //   //set ball friction
 
-      //once state is set to disable, dynamic interaction no longer calculated
-      body.setActivationState(STATE.DISABLE_DEACTIVATION);
+    //   //once state is set to disable, dynamic interaction no longer calculated
+    //   body.setActivationState(STATE.DISABLE_DEACTIVATION);
 
-      physicsWorld.addRigidBody(
-        body, //collisionGroupRedBall, collisionGroupGreenBall | collisionGroupPlane
-      );
+    //   physicsWorld.addRigidBody(
+    //     body, //collisionGroupRedBall, collisionGroupGreenBall | collisionGroupPlane
+    //   );
 
-      ball.userData.physicsBody = body;
-      ballObject.userData.physicsBody = body;
+    //   ball.userData.physicsBody = body;
+    //   ballObject.userData.physicsBody = body;
 
-      rigidBodies.push(ball);
-      rigidBodies.push(ballObject);
-    }
+    //   rigidBodies.push(ball);
+    //   rigidBodies.push(ballObject);
+    // }
 
     //create beach ball Mesh
-    function createBeachBall() {
-      let pos = { x: 20, y: 30, z: 0 };
-      let radius = 2;
-      let quat = { x: 0, y: 0, z: 0, w: 1 };
-      let mass = 20;
+    // function createBeachBall() {
+    //   let pos = { x: 20, y: 30, z: 0 };
+    //   let radius = 2;
+    //   let quat = { x: 0, y: 0, z: 0, w: 1 };
+    //   let mass = 20;
 
-      //import beach ball texture
-      var texture_loader = new THREE.TextureLoader(manager);
-      var beachTexture = texture_loader.load("./src/jsm/BeachBallColor.jpg");
-      beachTexture.wrapS = beachTexture.wrapT = THREE.RepeatWrapping;
-      beachTexture.repeat.set(1, 1);
-      beachTexture.anisotropy = 1;
-      beachTexture.encoding = THREE.sRGBEncoding;
+    //   //import beach ball texture
+    //   var texture_loader = new THREE.TextureLoader(manager);
+    //   var beachTexture = texture_loader.load("./src/jsm/BeachBallColor.jpg");
+    //   beachTexture.wrapS = beachTexture.wrapT = THREE.RepeatWrapping;
+    //   beachTexture.repeat.set(1, 1);
+    //   beachTexture.anisotropy = 1;
+    //   beachTexture.encoding = THREE.sRGBEncoding;
 
-      //threeJS Section
-      let ball = new THREE.Mesh(
-        new THREE.SphereGeometry(radius, 32, 32),
-        new THREE.MeshLambertMaterial({ map: beachTexture }),
-      );
+    //   //threeJS Section
+    //   let ball = new THREE.Mesh(
+    //     new THREE.SphereGeometry(radius, 32, 32),
+    //     new THREE.MeshLambertMaterial({ map: beachTexture }),
+    //   );
 
-      ball.position.set(pos.x, pos.y, pos.z);
-      ball.castShadow = true;
-      ball.receiveShadow = true;
-      scene.add(ball);
+    //   ball.position.set(pos.x, pos.y, pos.z);
+    //   ball.castShadow = true;
+    //   ball.receiveShadow = true;
+    //   scene.add(ball);
 
-      //Ammojs Section
-      let transform = new Ammo.btTransform();
-      transform.setIdentity();
-      transform.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
-      transform.setRotation(
-        new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w),
-      );
-      let motionState = new Ammo.btDefaultMotionState(transform);
+    //   //Ammojs Section
+    //   let transform = new Ammo.btTransform();
+    //   transform.setIdentity();
+    //   transform.setOrigin(new Ammo.btVector3(pos.x, pos.y, pos.z));
+    //   transform.setRotation(
+    //     new Ammo.btQuaternion(quat.x, quat.y, quat.z, quat.w),
+    //   );
+    //   let motionState = new Ammo.btDefaultMotionState(transform);
 
-      let colShape = new Ammo.btSphereShape(radius);
-      colShape.setMargin(0.05);
+    //   let colShape = new Ammo.btSphereShape(radius);
+    //   colShape.setMargin(0.05);
 
-      let localInertia = new Ammo.btVector3(0, 0, 0);
-      colShape.calculateLocalInertia(mass, localInertia);
+    //   let localInertia = new Ammo.btVector3(0, 0, 0);
+    //   colShape.calculateLocalInertia(mass, localInertia);
 
-      let rbInfo = new Ammo.btRigidBodyConstructionInfo(
-        mass,
-        motionState,
-        colShape,
-        localInertia,
-      );
-      let body = new Ammo.btRigidBody(rbInfo);
+    //   let rbInfo = new Ammo.btRigidBodyConstructionInfo(
+    //     mass,
+    //     motionState,
+    //     colShape,
+    //     localInertia,
+    //   );
+    //   let body = new Ammo.btRigidBody(rbInfo);
 
-      body.setRollingFriction(1);
-      physicsWorld.addRigidBody(body);
+    //   body.setRollingFriction(1);
+    //   physicsWorld.addRigidBody(body);
 
-      ball.userData.physicsBody = body;
-      rigidBodies.push(ball);
-    }
+    //   ball.userData.physicsBody = body;
+    //   rigidBodies.push(ball);
+    // }
 
     //create link boxes
-    function createBox(
-      x,
-      y,
-      z,
-      scaleX,
-      scaleY,
-      scaleZ,
-      boxTexture,
-      URLLink,
-      color = 0x000000,
-      transparent = true,
-    ) {
-      const boxScale = { x: scaleX, y: scaleY, z: scaleZ };
-      let quat = { x: 0, y: 0, z: 0, w: 1 };
-      let mass = 0; //mass of zero = infinite mass
+    // function createBox(
+    //   x,
+    //   y,
+    //   z,
+    //   scaleX,
+    //   scaleY,
+    //   scaleZ,
+    //   boxTexture,
+    //   URLLink,
+    //   color = 0x000000,
+    //   transparent = true,
+    // ) {
+    //   const boxScale = { x: scaleX, y: scaleY, z: scaleZ };
+    //   let quat = { x: 0, y: 0, z: 0, w: 1 };
+    //   let mass = 0; //mass of zero = infinite mass
 
-      //load link logo
-      const loader = new THREE.TextureLoader(manager);
-      const texture = loader.load(boxTexture);
-      texture.magFilter = THREE.LinearFilter;
-      texture.minFilter = THREE.LinearFilter;
-      texture.encoding = THREE.sRGBEncoding;
-      const loadedTexture = new THREE.MeshBasicMaterial({
-        map: texture,
-        transparent: transparent,
-        color: 0xffffff,
-      });
+    //   //load link logo
+    //   const loader = new THREE.TextureLoader(manager);
+    //   const texture = loader.load(boxTexture);
+    //   texture.magFilter = THREE.LinearFilter;
+    //   texture.minFilter = THREE.LinearFilter;
+    //   texture.encoding = THREE.sRGBEncoding;
+    //   const loadedTexture = new THREE.MeshBasicMaterial({
+    //     map: texture,
+    //     transparent: transparent,
+    //     color: 0xffffff,
+    //   });
 
-      var borderMaterial = new THREE.MeshBasicMaterial({
-        color: color,
-      });
-      borderMaterial.color.convertSRGBToLinear();
+    //   var borderMaterial = new THREE.MeshBasicMaterial({
+    //     color: color,
+    //   });
+    //   borderMaterial.color.convertSRGBToLinear();
 
-      var materials = [
-        borderMaterial, // Left side
-        borderMaterial, // Right side
-        borderMaterial, // Top side   ---> THIS IS THE FRONT
-        borderMaterial, // Bottom side --> THIS IS THE BACK
-        loadedTexture, // Front side
-        borderMaterial, // Back side
-      ];
+    //   var materials = [
+    //     borderMaterial, // Left side
+    //     borderMaterial, // Right side
+    //     borderMaterial, // Top side   ---> THIS IS THE FRONT
+    //     borderMaterial, // Bottom side --> THIS IS THE BACK
+    //     loadedTexture, // Front side
+    //     borderMaterial, // Back side
+    //   ];
 
-      const linkBox = new THREE.Mesh(
-        new THREE.BoxBufferGeometry(boxScale.x, boxScale.y, boxScale.z),
-        materials,
-      );
-      linkBox.position.set(x, y, z);
-      linkBox.renderOrder = 1;
-      linkBox.castShadow = true;
-      linkBox.receiveShadow = true;
-      linkBox.userData = { URL: URLLink, email: URLLink };
-      scene.add(linkBox);
-      objectsWithLinks.push(linkBox.uuid);
+    //   const linkBox = new THREE.Mesh(
+    //     new THREE.BoxBufferGeometry(boxScale.x, boxScale.y, boxScale.z),
+    //     materials,
+    //   );
+    //   linkBox.position.set(x, y, z);
+    //   linkBox.renderOrder = 1;
+    //   linkBox.castShadow = true;
+    //   linkBox.receiveShadow = true;
+    //   linkBox.userData = { URL: URLLink, email: URLLink };
+    //   scene.add(linkBox);
+    //   objectsWithLinks.push(linkBox.uuid);
 
-      addRigidPhysics(linkBox, boxScale);
+    //   addRigidPhysics(linkBox, boxScale);
 
-      cursorHoverObjects.push(linkBox);
-    }
+    //   cursorHoverObjects.push(linkBox);
+    // }
 
     //create Ammo.js body to add solid mass to "John Rao Software Engineer"
     function johnRaoWords(x, y, z) {
