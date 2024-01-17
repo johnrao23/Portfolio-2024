e8aac179a69c7f6c505dcce613cf98e3cb477dd4
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-01-17 14:55:20 -0500

    Update SceneSetup.tsx

--- src/3d/components/SceneSetup.tsx
+++ src/3d/components/SceneSetup.tsx
@@ -135,12 +135,12 @@ export const setupScene = (Ammo: any, container: HTMLDivElement, onLoaded: () =>
     createWallX(scene, Ammo, -125, 1.75, 0);
     createWallZ(scene, Ammo, 0, 1.75, 125);
     createWallZ(scene, Ammo, 0, 1.75, -125);
-    createBall(scene, Ammo);
-    createBeachBall(scene, Ammo);
-    createBox(scene, Ammo, 28, 2, -100, 4, 4, 1, boxTexture.Github, URL.gitHub, 0x000000, true);
-    createBox(scene, Ammo, 35.1, 2, -100, 4, 4, 1, boxTexture.LinkedIn, URL.LinkedIn, 0x0077b5, true);
-    createBox(scene, Ammo, 42.9, 2, -100, 4, 4, 1, boxTexture.twitter, URL.twitter, 0x00a2f4, true);
-    createBox(scene, Ammo, 50.5, 2, -100, 4, 4, 1, boxTexture.mail, "mailto:johnrao23@gmail.com", 0x000000, false);
+    createBall(scene, Ammo, loadingManager);
+    createBeachBall(scene, Ammo, loadingManager);
+    createBox(scene, Ammo, loadingManager, 28, 2, -100, 4, 4, 1, boxTexture.Github, URL.gitHub, 0x000000, true);
+    createBox(scene, Ammo, loadingManager, 35.1, 2, -100, 4, 4, 1, boxTexture.LinkedIn, URL.LinkedIn, 0x0077b5, true);
+    createBox(scene, Ammo, loadingManager, 42.9, 2, -100, 4, 4, 1, boxTexture.twitter, URL.twitter, 0x00a2f4, true);
+    createBox(scene, Ammo, loadingManager, 50.5, 2, -100, 4, 4, 1, boxTexture.mail, "mailto:johnrao23@gmail.com", 0x000000, false);
     createBillboard( scene, Ammo, -115, 2.5, -105, billboardTextures.terpSolutionsTexture, URL.terpsolutions, Math.PI * 0.2, );
     createBillboard( scene, Ammo, -80, 2.5, -110, billboardTextures.bullVsBearTexture, URL.bullVsBearTrading, Math.PI * 0.15, );
     createBillboard( scene, Ammo, -45, 2.5, -110, billboardTextures.fairbnbTexture, URL.getFairbnb, Math.PI * 0.1, );
@@ -205,7 +205,7 @@ export const setupScene = (Ammo: any, container: HTMLDivElement, onLoaded: () =>
       //check to see if ball escaped the plane
       if (ballObject.position.y < -50) {
         scene.remove(ballObject);
-        createBall(scene, Ammo);
+        createBall(scene, Ammo, loadingManager);
       }
     
       //check to see if ball is on text to rotate camera
