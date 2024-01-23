17c2bfe5a989ecea5058b1ee025c5f0719b189df
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-01-23 11:42:06 -0500

    Update ThreeContainer.tsx

--- src/3d/components/ThreeContainer.tsx
+++ src/3d/components/ThreeContainer.tsx
@@ -11,7 +11,6 @@ const ThreeContainer: React.FC = () => {
 
   const startButtonEventListener = () => {
     setShowOverlay(false);
-    setIsLoading(false);
     const preloadOverlay = document.getElementById("preload-overlay");
     if (preloadOverlay) {
       preloadOverlay.style.display = "none";
@@ -50,18 +49,23 @@ const ThreeContainer: React.FC = () => {
   if (isLoading) {
     return (
       <div className="preload-overlay">
-        <div className="start-page-content-div">
-          <h1 className="john-text postload">Hi, I'm <span className="yellow-text">John Rao!</span></h1>
-          <h1 className="postload start-page-text interactive-site-text">This is an interactive 3D site built with Three.js!</h1>
-          <h1 id="appDirections" className="start-page-text joystick-directions-text postload">Move the ball around with the arrow keys on the keyboard.</h1>
-          <button id="start-button" onClick={startButtonEventListener} className="postload">EXPLORE</button>
-        </div>
         <div className="trinity-rings-spinner">
           <div className="circle"></div>
           <div className="circle"></div>
           <div className="circle"></div>
         </div>
         <div className="loading-text-div">Loading<span className="loader__dot">.</span><span className="loader__dot">.</span><span className="loader__dot">.</span></div>
+      </div>
+    );
+  }
+
+  if (showOverlay) {
+    return (
+      <div className="start-page-content-div">
+        <h1 className="john-text postload">Hi, I'm <span className="yellow-text">John Rao!</span></h1>
+        <h1 className="postload start-page-text interactive-site-text">This is an interactive 3D site built with Three.js!</h1>
+        <h1 id="appDirections" className="start-page-text joystick-directions-text postload">Move the ball around with the arrow keys on the keyboard.</h1>
+        <button id="start-button" onClick={startButtonEventListener} className="postload">EXPLORE</button>
       </div>
     );
   }
