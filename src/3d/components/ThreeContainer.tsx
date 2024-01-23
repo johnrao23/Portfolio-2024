6e76c70e7fe03caa96de87daea750264d1b28154
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-01-23 12:07:43 -0500

    Update ThreeContainer.tsx

--- src/3d/components/ThreeContainer.tsx
+++ src/3d/components/ThreeContainer.tsx
@@ -5,20 +5,11 @@ import { createBeachBall } from './CreateObjects';
 
 let AmmoLib: any;
 
-
 const ThreeContainer: React.FC = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [showOverlay, setShowOverlay] = useState(true);
   const [ammoLoaded, setAmmoLoaded] = useState(false);
-
-  useEffect(() => {
-    if (containerRef.current) {
-      console.log("containerRef is attached to:", containerRef.current);
-    } else {
-      console.log("containerRef is null");
-    }
-  }, []);
 
   useEffect(() => {
     import('ammo.js').then(Ammo => {
@@ -49,33 +40,28 @@ const ThreeContainer: React.FC = () => {
     }, 1000);
   };
 
-  if (isLoading) {
-    return (
-      <div className="preload-overlay">
-        <div className="trinity-rings-spinner">
-          <div className="circle"></div>
-          <div className="circle"></div>
-          <div className="circle"></div>
-        </div>
-        <div className="loading-text-div">Loading<span className="loader__dot">.</span><span className="loader__dot">.</span><span className="loader__dot">.</span></div>
-      </div>
-    );
-  }
-
-  if (showOverlay) {
-    return (
-      <div className="start-page-content-div">
-        <h1 className="john-text postload">Hi, I'm <span className="yellow-text">John Rao!</span></h1>
-        <h1 className="postload start-page-text interactive-site-text">This is an interactive 3D site built with Three.js!</h1>
-        <h1 id="appDirections" className="start-page-text joystick-directions-text postload">Move the ball around with the arrow keys on the keyboard.</h1>
-        <button id="start-button" onClick={startButtonEventListener} className="postload">EXPLORE</button>
-      </div>
-    );
-  }
-  
-
   return (
-    <div ref={containerRef} style={{ width: '100%', height: '100%' }}>
+    <div>
+      {isLoading && (
+        <div className="preload-overlay">
+          <div className="trinity-rings-spinner">
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+          </div>
+          <div className="loading-text-div">Loading<span className="loader__dot">.</span><span className="loader__dot">.</span><span className="loader__dot">.</span></div>
+        </div>
+      )}
+      {showOverlay && !isLoading && (
+        <div className="start-page-content-div">
+          <h1 className="john-text postload">Hi, I'm <span className="yellow-text">John Rao!</span></h1>
+          <h1 className="postload start-page-text interactive-site-text">This is an interactive 3D site built with Three.js!</h1>
+          <h1 id="appDirections" className="start-page-text joystick-directions-text postload">Move the ball around with the arrow keys on the keyboard.</h1>
+          <button id="start-button" onClick={startButtonEventListener} className="postload">EXPLORE</button>
+        </div>
+      )}
+      <div ref={containerRef} style={{ width: '100%', height: '100%' }}>
+      </div>
     </div>
   );
 };
