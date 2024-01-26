8b17dae094386bd229ff82676af54ef2cefc82fc
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-01-26 10:09:49 -0500

    Update ThreeContainer.tsx

--- src/3d/components/ThreeContainer.tsx
+++ src/3d/components/ThreeContainer.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { setupScene } from './SceneSetup';
-import { launchClickPosition, launchHover } from './Utilities';
-import { createBeachBall } from './CreateObjects';
 import { useStore } from './store';
 
 const ThreeContainer: React.FC = () => {
@@ -22,20 +20,6 @@ const ThreeContainer: React.FC = () => {
     });
   }, [initializeAmmo]);
 
-  const startButtonEventListener = () => {
-    setShowOverlay(false);
-    const preloadOverlay = document.getElementById("preload-overlay");
-    if (preloadOverlay) {
-      preloadOverlay.style.display = "none";
-    }
-    document.removeEventListener("click", startButtonEventListener);
-    document.addEventListener("click", launchClickPosition);
-    createBeachBall();
-    setTimeout(() => {
-      document.addEventListener("mousemove", launchHover);
-    }, 1000);
-  };
-
   return (
     <div>
       {isLoading && (
@@ -46,14 +30,6 @@ const ThreeContainer: React.FC = () => {
             <div className="circle"></div>
           </div>
           <div className="loading-text-div">Loading<span className="loader__dot">.</span><span className="loader__dot">.</span><span className="loader__dot">.</span></div>
-        </div>
-      )}
-      {showOverlay && !isLoading && (
-        <div className="start-page-content-div">
-          <h1 className="john-text postload">Hi, I'm <span className="yellow-text">John Rao!</span></h1>
-          <h1 className="postload start-page-text interactive-site-text">This is an interactive 3D site built with Three.js!</h1>
-          <h1 id="appDirections" className="start-page-text joystick-directions-text postload">Move the ball around with the arrow keys on the keyboard.</h1>
-          <button id="start-button" onClick={startButtonEventListener} className="postload">EXPLORE</button>
         </div>
       )}
       <div ref={containerRef} style={{ width: '100%', height: '100%' }}>
