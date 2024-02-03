5b2bb588fb8ef63467fb12601f7f46fe3006a636
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-02-02 20:49:58 -0500

    Update ThreeContainer.tsx

--- src/3d/components/ThreeContainer.tsx
+++ src/3d/components/ThreeContainer.tsx
@@ -51,7 +51,7 @@ const ThreeContainer: React.FC = () => {
   };
 
   return (
-    <>
+    <div ref={containerRef} style={{ width: '100%', height: '100%' }}>
     {isLoading && (
       <div className="preload-overlay">
         <div className="trinity-rings-spinner">
@@ -62,7 +62,7 @@ const ThreeContainer: React.FC = () => {
         <div className="loading-text-div">Loading<span className="loader__dot">.</span><span className="loader__dot">.</span><span className="loader__dot">.</span></div>
       </div>
     )}
-      {!isLoading && showOverlay && (
+      {showOverlay && (
         <div className="start-page-content-div">
           <h1 className="john-text">Hi, I'm <span className="yellow-text">John Rao!</span></h1>
           <h1 className="start-page-text interactive-site-text">This is an interactive 3D site built with Three.js!</h1>
@@ -70,9 +70,7 @@ const ThreeContainer: React.FC = () => {
           <button id="start-button" onClick={startButtonEventListener}>EXPLORE</button>
         </div>
       )}
-      <div ref={containerRef} style={{ width: '100%', height: '100%' }}>
     </div>
-    </>
   );
 };
 
