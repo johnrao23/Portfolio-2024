d1c12e2f973f6fd29d611834e3b7d4b12c15b7dc
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-02-16 12:54:08 -0500

    Update fragment.glsl

--- src/3d/shaders/fragment.glsl
+++ src/3d/shaders/fragment.glsl
@@ -1,28 +1,22 @@
 varying vec3 vColor;
-void main()
-{
-     // Disc
-     /*
-    float strength = distance(gl_PointCoord, vec2(0.5));
-    strength = step(0.5, strength);
-    strength = 1.0 - strength;*/
 
-    // Diffuse point
-    /*
-    float strength = distance(gl_PointCoord, vec2(0.5));
-    strength *= 2.0;
-    strength = 1.0 - strength;*/
+// Function to convert sRGB to Linear
+vec3 sRGBToLinear(vec3 color) {
+    return pow(color, vec3(2.2));
+}
 
-     // Light point
-    // Light point
+void main() {
+    // Convert vColor from sRGB to Linear space for correct lighting calculations
+    vec3 linearColor = sRGBToLinear(vColor);
+
     float strength = distance(gl_PointCoord, vec2(0.5));
     strength = 1.0 - strength;
     strength = pow(strength, 10.0);
 
-    // Final color
-    vec3 color = mix(vec3(0.0), vColor, strength);
-    gl_FragColor = vec4(color, 1.0);
+    // Mix the linearColor with some value in linear space
+    vec3 finalColor = mix(vec3(0.0), linearColor, strength);
 
-    // gl_FragColor = vec4(gl_PointCoord, 1.0, 1.0);   // we already have access to the UV in the fragment shader with gl_PointCoord
-    
+    // Since Three.js automatically converts linear to sRGB space if configured,
+    // we output the color in linear space
+    gl_FragColor = vec4(finalColor, 1.0);
 }
