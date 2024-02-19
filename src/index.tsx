476250fc920508fc2dfa2fc898ed8f8a8c4ce91d
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-02-19 13:52:05 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom'; // Import Router
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './static/App';
 import Main from './3d/Main';
 import WEBGL from './3d/components/WebGL';
@@ -8,20 +8,10 @@ import WEBGL from './3d/components/WebGL';
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 const pathname = window.location.pathname;
 
-if (pathname.startsWith('/static/*') || !WEBGL.isWebGLAvailable()) {
-  root.render(
-    <React.StrictMode>
-      <Router>
-        <App />
-      </Router>
-    </React.StrictMode>
-  );
-} else {
-  root.render(
-    <React.StrictMode>
-      <Router>
-        <Main />
-      </Router>
-    </React.StrictMode>
-  );
-}
+root.render(
+  <React.StrictMode>
+    <Router>
+      {pathname.startsWith('/static') || !WEBGL.isWebGLAvailable() ? <App /> : <Main />}
+    </Router>
+  </React.StrictMode>
+);
