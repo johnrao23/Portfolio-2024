c88e4689b9b7238031a94183ce9b3eef7903d996
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-03-06 10:59:43 -0500

    Update Main.tsx

--- src/static/components/Main.tsx
+++ src/static/components/Main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import clsx from 'clsx';
 import NavBar from "./NavBar";
 import { Footer } from "./Footer";
 import { GitHubIcon, InstagramIcon, LinkedInIcon, TwitterIcon } from "./SocialIcons";
@@ -10,6 +11,47 @@ import image4 from './photos/image-4.jpg';
 import image5 from './photos/image-5.jpg';
 
 const Main: React.FC = () => {
+
+    function SocialLink({
+        icon: Icon,
+        ...props
+        }: React.ComponentPropsWithoutRef<typeof Link> & {
+        icon: React.ComponentType<{ className?: string }>
+        }) {
+        return (
+            <Link className="group -m-1 p-1" {...props}>
+            <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
+            </Link>
+        )
+    }
+
+    function Photos() {
+        const rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2'];
+        const images = [JRThreeImg, image2, image3, image4, image5];
+      
+        return (
+          <div className="mt-16 sm:mt-20">
+            <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+              {images.map((image, index) => (
+                <div
+                  key={index}
+                  className={clsx(
+                    'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
+                    rotations[index % rotations.length],
+                  )}
+                >
+                  <img
+                    src={image}
+                    alt=""
+                    className="absolute inset-0 h-full w-full object-cover"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        );
+      }
+
     return (
         <div>
             <NavBar />
