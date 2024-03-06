ee78c732c6b480d69cf410f911d3aaf096f16c8b
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-03-06 11:58:48 -0500

    Update Main.tsx

--- src/static/components/Main.tsx
+++ src/static/components/Main.tsx
@@ -23,7 +23,7 @@ const Main: React.FC = () => {
         }) {
 
         return (
-            <Link to={to} className="group -m-1 p-1" aria-label={ariaLabel}> // Changed `href` to `to` and added `aria-label`
+            <Link to={to} className="group -m-1 p-1" aria-label={ariaLabel}>
                 <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
             </Link>
         )
