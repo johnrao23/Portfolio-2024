d48bcdc2f8eec9054404a9256e7f2d7535f5b856
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-03-06 10:55:33 -0500

    Update Main.tsx

--- src/static/components/Main.tsx
+++ src/static/components/Main.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import NavBar from "./NavBar";
 import { Footer } from "./Footer";
 import { GitHubIcon, InstagramIcon, LinkedInIcon, TwitterIcon } from "./SocialIcons";
-import image1 from './photos/image-1.jpg';
+import JRThreeImg from "../assets/3dsite.png";
 import image2 from './photos/image-2.jpg';
 import image3 from './photos/image-3.jpg';
 import image4 from './photos/image-4.jpg';
