67d6ea35f3316112687b1d723c046a717b44c69f
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-03-06 11:30:03 -0500

    Update NavBar.tsx

--- src/static/components/NavBar.tsx
+++ src/static/components/NavBar.tsx
@@ -104,7 +104,7 @@ function DesktopNavigation() {
   return (
     <nav>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
-        <NavItem href="/about">About</NavItem>
+        <NavItem href="/static/about">About</NavItem>
         <NavItem href="/articles">Articles</NavItem>
         <NavItem href="/projects">Projects</NavItem>
         <NavItem href="/speaking">Speaking</NavItem>
@@ -124,7 +124,7 @@ function MobileNavigation() {
       </button>
       {isOpen && (
         <ul className="flex flex-col items-center">
-          <NavItem href="/about">About</NavItem>
+          <NavItem href="/static/about">About</NavItem>
           <NavItem href="/articles">Articles</NavItem>
           <NavItem href="/projects">Projects</NavItem>
           <NavItem href="/speaking">Speaking</NavItem>
