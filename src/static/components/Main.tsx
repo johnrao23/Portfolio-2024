df9b3d8b10930ad770910196fd1741f2444ce44a
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-03-06 11:15:18 -0500

    Update Main.tsx

--- src/static/components/Main.tsx
+++ src/static/components/Main.tsx
@@ -18,7 +18,7 @@ const Main: React.FC = () => {
         }: React.ComponentPropsWithoutRef<typeof Link> & {
         icon: React.ComponentType<{ className?: string }>
         }) {
-            
+
         return (
             <Link className="group -m-1 p-1" {...props}>
                 <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
@@ -56,70 +56,42 @@ const Main: React.FC = () => {
     return (
         <div>
             <NavBar />
-            <h1>Main Content</h1>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
-            <h2>This will be a long page with navbar jumping to page sections</h2>
+            <div className="mt-9">
+                <div className="max-w-2xl">
+                    <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+                        Software designer, founder, and amateur astronaut.
+                    </h1>
+                    <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+                        I’m Spencer, a software designer and entrepreneur based in New York
+                        City. I’m the founder and CEO of Planetaria, where we develop
+                        technologies that empower regular people to explore space on their
+                        own terms.
+                    </p>
+                    <div className="mt-6 flex gap-6">
+                        <SocialLink
+                            href="https://twitter.com"
+                            ariaLabel="Follow on Twitter"
+                            icon={TwitterIcon}
+                            />
+                        <SocialLink
+                            href="https://instagram.com"
+                            ariaLabel="Follow on Instagram"
+                            icon={InstagramIcon}
+                            />
+                        <SocialLink
+                            href="https://github.com"
+                            ariaLabel="Follow on GitHub"
+                            icon={GitHubIcon}
+                            />
+                        <SocialLink
+                            href="https://linkedin.com"
+                            ariaLabel="Follow on LinkedIn"
+                            icon={LinkedInIcon}
+                            />
+                    </div>
+                </div>
+            </div>
+            <Photos />
             <Link to="/static/about" style={{ textDecoration: 'none' }}>
                 <button>About Page</button>
             </Link>
