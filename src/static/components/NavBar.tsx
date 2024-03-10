5c923cffed19c2601bb2d34753db62483df007e9
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-03-10 15:01:13 -0400

    Update NavBar.tsx

--- src/static/components/NavBar.tsx
+++ src/static/components/NavBar.tsx
@@ -105,10 +105,10 @@ function DesktopNavigation() {
     <nav>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <NavItem href="/static/about">About</NavItem>
-        <NavItem href="/articles">Articles</NavItem>
-        <NavItem href="/projects">Projects</NavItem>
-        <NavItem href="/speaking">Speaking</NavItem>
-        <NavItem href="/uses">Uses</NavItem>
+        <NavItem href="/static">Projects</NavItem>
+        <NavItem href="/static">Experience</NavItem>
+        <NavItem href="/static">Skills</NavItem>
+        <NavItem href="/static">Hobbies</NavItem>
       </ul>
     </nav>
   );
@@ -125,10 +125,10 @@ function MobileNavigation() {
       {isOpen && (
         <ul className="flex flex-col items-center">
           <NavItem href="/static/about">About</NavItem>
-          <NavItem href="/articles">Articles</NavItem>
-          <NavItem href="/projects">Projects</NavItem>
-          <NavItem href="/speaking">Speaking</NavItem>
-          <NavItem href="/uses">Uses</NavItem>
+          <NavItem href="/static">Projects</NavItem>
+          <NavItem href="/static">Experience</NavItem>
+          <NavItem href="/static">Skills</NavItem>
+          <NavItem href="/static">Hobbies</NavItem>
         </ul>
       )}
     </div>
