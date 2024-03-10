84ed2eeedeb0575cd77c1d14b83ce06d881399c5
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-03-10 12:13:14 -0400

    Update About.tsx

--- src/static/components/About.tsx
+++ src/static/components/About.tsx
@@ -39,7 +39,7 @@ export default function About() {
         <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
           <div className="lg:pl-20">
             <div className="max-w-xs px-2.5 lg:max-w-none">
-              <Image
+              <img
                 src={portraitImage}
                 alt=""
                 sizes="(min-width: 1024px) 32rem, 20rem"
