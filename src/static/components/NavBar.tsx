3f9d63b5006f2dcc78f264e1cb051980efed84ee
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-03-10 15:24:10 -0400

    Update NavBar.tsx

--- src/static/components/NavBar.tsx
+++ src/static/components/NavBar.tsx
@@ -90,7 +90,8 @@ function NavItem({
     to: string
     children: React.ReactNode
   }) {
-    let isActive = usePathname() === to
+    const location = useLocation();
+    const isActive = location.pathname === to;
 
   return (
     <li>
@@ -112,9 +113,9 @@ function NavItem({
   )
 }
 
-function DesktopNavigation() {
+function DesktopNavigation(props: React.ComponentPropsWithoutRef<'nav'>) {
   return (
-    <nav>
+    <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <NavItem to="/static/about">About</NavItem>
         <NavItem to="/static">Projects</NavItem>
