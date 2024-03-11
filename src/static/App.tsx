3cad40712229a75e4d3216982ccaae8423d98e54
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-03-11 11:35:23 -0400

    Update App.tsx

--- src/static/App.tsx
+++ src/static/App.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Main from './components/Main';
 import "../index-static.css"
 import About from './components/About';
+import Projects from './components/Projects';
 
 const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
     </Routes>
   );
 };
