2de3de6f211ae952671c26c72600235b61c21c5f
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-03-11 12:07:13 -0400

    Update NavBar.tsx

--- src/static/components/NavBar.tsx
+++ src/static/components/NavBar.tsx
@@ -110,12 +110,10 @@ function Avatar({
           'rounded-full bg-zinc-100 object-cover dark:bg-zinc-800',
           large ? 'h-16 w-16' : 'h-9 w-9',
         )}
-        priority
       />
     </Link>
   )
 }
-
 
 // Navigation items
 function NavItem({
