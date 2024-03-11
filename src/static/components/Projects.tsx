ea26b30923560616614acb03f03106c31021ac96
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-03-11 11:34:31 -0400

    Update Projects.tsx

--- src/static/components/Projects.tsx
+++ src/static/components/Projects.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Projects = () => {
-    return
+    return (
+        <div>
+            <h1>Projects Section</h1>
+        </div>
+    )
 }
 
 export default Projects;
