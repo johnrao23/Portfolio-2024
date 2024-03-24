e011f7209cdbc9305f432c864a487cb8e8e49f27
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-03-24 14:45:54 -0400

    404

--- src/static/components/Button.tsx
+++ src/static/components/Button.tsx
@@ -1,34 +1,36 @@
+import React from 'react';
 import { Link } from "react-router-dom";
-import clsx from 'clsx'
+import clsx from 'clsx';
 
 const variantStyles = {
-  primary:
-    'bg-zinc-800 font-semibold text-zinc-100 hover:bg-zinc-700 active:bg-zinc-800 active:text-zinc-100/70 dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-700 dark:active:text-zinc-100/70',
-  secondary:
-    'bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70',
-}
+  primary: 'bg-zinc-800 font-semibold text-zinc-100 hover:bg-zinc-700 active:bg-zinc-800 active:text-zinc-100/70 dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-700 dark:active:text-zinc-100/70',
+  secondary: 'bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70',
+};
 
 type ButtonProps = {
-  variant?: keyof typeof variantStyles
+  variant?: keyof typeof variantStyles;
+  className?: string;
+  children?: React.ReactNode;
 } & (
-  | (React.ComponentPropsWithoutRef<'button'> & { href?: undefined })
-  | React.ComponentPropsWithoutRef<typeof Link>
-)
+  | ({ to: string; } & Omit<React.ComponentPropsWithoutRef<typeof Link>, 'to'>)
+  | React.ButtonHTMLAttributes<HTMLButtonElement>
+);
 
-export function Button({
-  variant = 'primary',
-  className,
-  ...props
-}: ButtonProps) {
+export const Button: React.FC<ButtonProps> = ({ variant = 'primary', className, children, ...props }) => {
+  const isLink = 'to' in props;
   className = clsx(
     'inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none',
     variantStyles[variant],
     className,
-  )
+  );
 
-  return typeof props.href === 'undefined' ? (
-    <button className={className} {...props} />
+  return isLink ? (
+    <Link {...(props as React.ComponentPropsWithoutRef<typeof Link>)} className={className}>
+      {children}
+    </Link>
   ) : (
-    <Link className={className} {...props} />
-  )
-}
+    <button {...(props as React.ButtonHTMLAttributes<HTMLButtonElement>)} className={className}>
+      {children}
+    </button>
+  );
+};
