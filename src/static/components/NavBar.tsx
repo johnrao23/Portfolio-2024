676487bf0c8b7bd51624bdcf60df39c9d3d53dd1
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-03-25 12:30:12 -0400

    Update NavBar.tsx

--- src/static/components/NavBar.tsx
+++ src/static/components/NavBar.tsx
@@ -248,11 +248,13 @@ const ThemeToggle = () => {
       className="group rounded-full bg-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
       onClick={toggleTheme}
     >
-      {theme === 'light' ? (
-        <SunIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600" />
-      ) : (
-        <MoonIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:block [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400 [@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-teal-500" />
-      )}
+      {
+        theme === 'light' ? (
+          <SunIcon className="h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden" />
+        ) : (
+          <MoonIcon className="h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block hover:fill-zinc-200 hover:stroke-zinc-400" />
+        )
+      }
     </button>
   );
 };
