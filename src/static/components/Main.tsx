30a120822d3a1749b25791cf00634ff2ac943422
John Rao <86802699+johnrao23@users.noreply.github.com>
2024-05-01 15:51:06 -0400

    Update Main.tsx

--- src/static/components/Main.tsx
+++ src/static/components/Main.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import clsx from "clsx";
 import { Container } from "./Container";
 import { Button } from "./Button";
-import Speaking from "./Experience";
 import { GitHubIcon, GalaxyIcon, LinkedInIcon, XIcon } from "./SocialIcons";
 import { BriefcaseIcon, ArrowDownIcon, MailIcon } from "./MiscIcons";
 import {
@@ -17,6 +16,9 @@ import FairbnbImg from "../assets/fairbnb.png";
 import LNImg from "../assets/LucasNoahPic.jpeg";
 import NYCImg from "../assets/NYC.png";
 import SDImg from "../assets/DeskSetup.webp";
+import { Card } from "./Card";
+import { Section } from "./Section";
+import { SimpleLayout } from "./SimpleLayout";
 
 const Main: React.FC = () => {
   function SocialLink({
@@ -230,6 +232,88 @@ const Main: React.FC = () => {
             </div>
           ))}
         </div>
+      </div>
+    );
+  }
+
+  function SpeakingSection({
+    children,
+    ...props
+  }: React.ComponentPropsWithoutRef<typeof Section>) {
+    return (
+      <Section {...props}>
+        <div className="space-y-16">{children}</div>
+      </Section>
+    );
+  }
+
+  function Appearance({
+    title,
+    description,
+    event,
+    cta,
+    href,
+  }: {
+    title: string;
+    description: string;
+    event: string;
+    cta: string;
+    href: string;
+  }) {
+    return (
+      <Card as="article">
+        <Card.Title as="h3" href={href}>
+          {title}
+        </Card.Title>
+        <Card.Eyebrow decorate>{event}</Card.Eyebrow>
+        <Card.Description>{description}</Card.Description>
+        <Card.Cta>{cta}</Card.Cta>
+      </Card>
+    );
+  }
+
+  function Speaking() {
+    return (
+      <div className="space-y-20">
+        <SpeakingSection title="Conferences">
+          <Appearance
+            href="#"
+            title="In space, no one can watch you stream — until now"
+            description="A technical deep-dive into HelioStream, the real-time streaming library I wrote for transmitting live video back to Earth."
+            event="SysConf 2021"
+            cta="Watch video"
+          />
+          <Appearance
+            href="#"
+            title="Lessons learned from our first product recall"
+            description="They say that if you’re not embarassed by your first version, you’re doing it wrong. Well when you’re selling DIY space shuttle kits it turns out it’s a bit more complicated."
+            event="Business of Startups 2020"
+            cta="Watch video"
+          />
+        </SpeakingSection>
+        <SpeakingSection title="Podcasts">
+          <Appearance
+            href="#"
+            title="Using design as a competitive advantage"
+            description="How we used world-class visual design to attract a great team, win over customers, and get more press for Planetaria."
+            event="Encoding Design, July 2022"
+            cta="Listen to podcast"
+          />
+          <Appearance
+            href="#"
+            title="Bootstrapping an aerospace company to $17M ARR"
+            description="The story of how we built one of the most promising space startups in the world without taking any capital from investors."
+            event="The Escape Velocity Show, March 2022"
+            cta="Listen to podcast"
+          />
+          <Appearance
+            href="#"
+            title="Programming your company operating system"
+            description="On the importance of creating systems and processes for running your business so that everyone on the team knows how to make the right decision no matter the situation."
+            event="How They Work Radio, September 2021"
+            cta="Listen to podcast"
+          />
+        </SpeakingSection>
       </div>
     );
   }
